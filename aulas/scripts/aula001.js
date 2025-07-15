6d1d88233576d8d3aab4e58839dabd5ebc663038
hiago <127118134+hiagoPilar@users.noreply.github.com>
2025-07-15 23:07:16 +0100

    .

--- aulas/scripts/aula001.js
+++ aulas/scripts/aula001.js
@@ -128,6 +128,13 @@ Operadores relacionais:
     - Operador de não identidade: !==
 
 
+
+Operador ternário:
+    let idade = 18; 
+    let podeDirigir = (idade >= 18) ? "Pode dirigir" : "Não pode dirigir";
+
+
+
 */   
 
 
