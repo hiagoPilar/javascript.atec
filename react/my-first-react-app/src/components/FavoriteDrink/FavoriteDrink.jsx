fc95c49d71e22bcf4e4ab0caf8e6158ad3b8665c
Hiago <hiago@icloud.com>
2025-09-04 14:17:54 -0300

    .

---
+++ react/my-first-react-app/src/components/FavoriteDrink/FavoriteDrink.jsx
@@ -0,0 +1,5 @@
+function FavoriteDrink(){
+    return <h3>My favorite drink is water.</h3>
+}
+
+export default FavoriteDrink
---
+++ react/my-first-react-app/src/components/FavoriteFood/FavoriteFood.jsx
@@ -0,0 +1,5 @@
+function FavoriteFood(){
+    return <h3>My favorite food is bread.</h3>
+}
+
+export default FavoriteFood
